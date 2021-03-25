2230013aeae035f724e8a5bbf1e57df927e6d068
Yan Sapta <79567238+yanSapta@users.noreply.github.com>
2021-03-25 12:56:59 +0700

    Modified status bar

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -6,7 +6,7 @@ function Splash() {
             <div className="container mx-auto  min-h-screen">
                 <div className="flex flex-col items-center justify-center h-screen">
                     <div className="w-full md:w-4/12 text-center">
-                        <image src={logo} alt="LuxSpace | Home Furniture" className="mx-auto mb=8"/>
+                        <img src={logo} alt="LuxSpace | Home Furniture" className="mx-auto mb=8"/>
                         <p className="mb-16 px-4">
                             Kami menyediakan furniture berkelas untuk rumah anda, jadikan rumah anda lebih nyaman.
                         </p>
