a1895abfdb8d832ac08a1ef0d81206015e554c5e
Yan Sapta <79567238+yanSapta@users.noreply.github.com>
2021-03-25 12:04:27 +0700

    Offline status & pwa manifes baru

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Header from './components/Header.js';
 import Hero from './components/Hero.js';
 import AsideMenu from './components/AsideMenu.js';
 import Footer from './components/Footer.js';
+import Offline from './components/Offline.js';
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
+
+  function handleOfflineStatus(){
+    setOfflineStatus(!navigator.onLine);
+  }
 
   React.useEffect(function(){
     (async function(){
@@ -20,10 +26,26 @@ function App() {
       });
       const {nodes} = await response.json();
       setItems (nodes);
+
+      const script = document.createElement("script"); //membuat element untuk loding carousel
+      script.src = "/carousel.js";
+      script.async = false;
+      document.body.appendChild(script);
     })();
-  },[])
+
+    handleOfflineStatus();
+    window.addEventListener('online',handleOfflineStatus);
+    window.addEventListener('offline',handleOfflineStatus);
+
+    return function(){
+      window.removeEventListener('online',handleOfflineStatus);
+      window.removeEventListener('offline',handleOfflineStatus);
+    }
+
+  },[offlineStatus])
   return (
     <>
+    {offlineStatus && <Offline />}
     <Header/>
     <Hero/>
     <Browse/>
